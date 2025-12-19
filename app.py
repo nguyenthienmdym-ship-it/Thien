4655d4c9719dbfd710d03c1f3aa574c4171bfdbe
nguyenthienmdym-ship-it <nguyenthienmdym@gmail.com>
2025-12-19 08:25:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Sùng Văn Quý - Trường PTDTBT Tiểu học Đường Thượng - ĐT: 097 4109 364</p>
+    <p>Tác giả: Nguyễn Thị Thiện - Trường PTDTBT Tiểu học Mậu Long - ĐT: 0366414300</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Sùng Văn Quý - Trường PTDTBT Tiểu học Đường Thượng - ĐT: 097 4109 364</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Nguyễn Thị Thiện - Trường PTDTBT Tiểu học Mậu Long - ĐT: 0366414300</div>", unsafe_allow_html=True)
